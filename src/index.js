c574bf0964dc75c4c452e5cfb97dffc5eb8a0020
AntonLovkin <anton28111987@gmail.com>
2021-05-12 22:39:55 +0300

    рабочий хендлбарс
    если замержить, то можно увидеть события полученные с API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './sass/main.scss';
+import cardsTpl from ".././src/cards.hbs";
+
+const refs = {
+    cardContainer: document.querySelector('.main-section-event-list')
+}
+
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
+const API_KEY = 'CFtAODian0BKODBcnwTSMVzdAwRsrdku';
+
+function fetchEvents() {
+
+    fetch(`${BASE_URL}/events.json?apikey=${API_KEY}`)
+        .then(rawResult => rawResult.json())
+        .then(card => {
+            console.log(card);
+            const markup = cardsTpl(card);
+            console.log(markup);
+            refs.cardContainer.innerHTML = markup;
+        })
+}
+fetchEvents()
