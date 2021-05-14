5cdefe95d2ddc0e43d7cd46906b6d9d83c68372c
AntonLovkin <anton28111987@gmail.com>
2021-05-14 23:43:19 +0300

    создал файл modal.hbs

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import './sass/main.scss';
-import cardsTpl from ".././src/cards.hbs";
+import cardsTpl from ".././src/markup/cards.hbs";
+import modalTpl from ".././src/markup/modal.hbs";
 
 const refs = {
-    cardContainer: document.querySelector('.main-section-event-list')
+    cardContainer: document.querySelector('.main-section-event-list'),
+    modalContainer: document.querySelector('.js-modal')
 }
+console.log(modalTpl);
+console.log(refs.modalContainer);
+refs.modalContainer.innerHTML = modalTpl();
 
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
 const API_KEY = 'CFtAODian0BKODBcnwTSMVzdAwRsrdku';
 
 function fetchEvents() {
 
+    // fetch(`${BASE_URL}/events.json?apikey=${API_KEY}`)
+    //     .then(rawResult => rawResult.json())
+    //     .then(card => {
+    //         console.log(card);
+    //         const markup = cardsTpl(card);
+    //         console.log(markup);
+    //         refs.cardContainer.innerHTML = markup;
+    //     })
     fetch(`${BASE_URL}/events.json?apikey=${API_KEY}`)
         .then(rawResult => rawResult.json())
         .then(card => {
             console.log(card);
-            const markup = cardsTpl(card);
+            const markup = modalTpl(card);
             console.log(markup);
-            refs.cardContainer.innerHTML = markup;
+            refs.modalContainer.innerHTML = markup;
         })
 }
 fetchEvents()
