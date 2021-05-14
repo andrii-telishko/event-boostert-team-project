d963002e25e7e2aedc734df90e7ee96cc8e9c5bf
Andrii Telishko <telishkoav@gmail.com>
2021-05-14 16:08:02 +0300

    оформив код

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './sass/main.scss';
 import cardsTpl from "./templates/eventCard.hbs";
-// import './js/fitchEvents.js'
-
-const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
-const API_KEY = 'CFtAODian0BKODBcnwTSMVzdAwRsrdku';
+import refs from './js/refs';
+import constants from './js/constants.js';
+import './js/fetchEvents.js';
+import fetchingForm from './js/fetchEvents'
+import { debounce } from "lodash";
 
 function fetchEvents() {
 
-    fetch(`${BASE_URL}/events.json?apikey=${API_KEY}`)
+    fetch(`${constants.BASE_URL}/events.json?apikey=${constants.API_KEY}`)
         .then(rawResult => rawResult.json())
         .then(card => {
             console.log(card);
@@ -17,3 +18,15 @@ function fetchEvents() {
         })
 }
 fetchEvents()
+
+refs.chooseCountryInput.addEventListener('change', onSearchEventByCountry)
+
+function onSearchEventByCountry(e) {
+    fetchingForm.fetchEventsInForm(e.target.value, refs.searchingInput.value)
+    }
+    
+refs.searchingInput.addEventListener('input', debounce(onSearchEvent, 1000))
+
+function onSearchEvent (e) {
+    fetchingForm.fetchEventsInForm(refs.chooseCountryInput.value, e.target.value)
+        }
