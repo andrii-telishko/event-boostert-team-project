669774795a56f2b1b4876c2bda386885ca034143
RadIgorGo <70336756+RadIgorGo@users.noreply.github.com>
2021-05-16 14:28:02 +0300

    Наче працює.

--- src/js/fetchAllEvents.js
+++ src/js/fetchAllEvents.js
@@ -1,35 +1,24 @@
-import refs from './refs'
-import cardsTpl from "../templates/eventCard.hbs";
-import modalTpl from "../templates/modal.hbs";
+import refs from './refs';
+import cardsTpl from '../templates/eventCard.hbs';
+import modalTpl from '../templates/modal.hbs';
 import constants from '../js/constants';
 
 export function fetchAllEvents() {
-    fetch(`${constants.BASE_URL}/events.json?apikey=${constants.API_KEY}`)
-        .then(rawResult => rawResult.json())
-        .then(card => {
+  fetch(`${constants.BASE_URL}/events.json?apikey=${constants.API_KEY}`)
+    .then(rawResult => rawResult.json())
+    .then(card => {
+      // console.log(card);
+      const markup = cardsTpl(card);
+      // console.log(markup);
+      refs.cardContainer.innerHTML = markup;
+    });
 
-            // console.log(card);
-            const markup = cardsTpl(card);
-            // console.log(markup);
-            refs.cardContainer.innerHTML = markup;
-        })
-
-         fetch(`${constants.BASE_URL}/events.json?apikey=${constants.API_KEY}`)
-        .then(rawResult => rawResult.json())
-        .then(card => {
-            // console.log(card);
-            const markup = modalTpl(card);
-            // console.log(markup);
-            refs.modalContainer.innerHTML = markup;
-        })
+  fetch(`${constants.BASE_URL}/events.json?apikey=${constants.API_KEY}`)
+    .then(rawResult => rawResult.json())
+    .then(card => {
+      // console.log(card);
+      const markup = modalTpl(card);
+      // console.log(markup);
+      refs.modalContainer.innerHTML = markup;
+    });
 }
-    
-
-
-            const markup = cardsTpl(card);
-            refs.cardContainer.innerHTML = markup;
-        })
-}
-        
-
-    
