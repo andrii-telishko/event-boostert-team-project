7f8ee96f0147a51f10813a4f15f8c59f63c63c7d
Andrii Telishko <telishkoav@gmail.com>
2021-05-17 21:58:14 +0300

    додав дів у модал

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,9 +2,10 @@ export default {
     cardContainer: document.querySelector('.main-section-event-list'),
     chooseCountryInput: document.querySelector('.choose-country'),
     searchingInput: document.querySelector('.form-field'),
-    modalContainer: document.querySelector('.js-modal'),
+    modalContainer: document.querySelector('.template-modal-conteiner'),
 
     eventClick: document.querySelector('.js-list'),
-showModal: document.querySelector('.backdrop')
+    showModal: document.querySelector('.backdrop'),
+    
 
 }
