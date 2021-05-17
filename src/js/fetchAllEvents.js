ad6ed00b7bbec6d37afa666c5ae8de046e59de5a
AntonLovkin <anton28111987@gmail.com>
2021-05-17 22:37:59 +0300

    ddhdh

--- src/js/fetchAllEvents.js
+++ src/js/fetchAllEvents.js
@@ -3,10 +3,10 @@ import cardsTpl from "../templates/eventCard.hbs";
 import modalTpl from "../templates/modal.hbs";
 import constants from '../js/constants';
 
-export default {
+ export default {
     function: fetchAllEvents(),
 }
-  
+
 function fetchAllEvents() {
     fetch(`${constants.BASE_URL}/events.json?apikey=${constants.API_KEY}`)
         .then(rawResult => rawResult.json())
@@ -26,7 +26,7 @@ function fetchAllEvents() {
             refs.modalContainer.innerHTML = markup;
         })
 }
-    
+  
 
         
 
